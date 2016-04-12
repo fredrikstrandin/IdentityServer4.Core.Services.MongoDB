21c75790b3d865302d7b1af3e40081ec639fd864
Fredrik Strandin <strandin72@gmail.com>
2016-04-12 21:19:25 +0200

    Add Log to file and Eventlog.

--- Sample/IdSvrHost/Startup.cs
+++ Sample/IdSvrHost/Startup.cs
@@ -14,6 +14,7 @@ using System.IO;
 using IdentityServer4.Core.Services.MongoDB;
 using MongoDB.Driver;
 using IdSvrHost.UI.Login;
+using System.Diagnostics;
 
 namespace IdSvrHost2
 {
@@ -29,7 +30,7 @@ namespace IdSvrHost2
         public void ConfigureServices(IServiceCollection services)
         {
             var _client = new MongoClient();
-            
+
             services.AddInstance<IMongoDatabase>(_client.GetDatabase("IdentityServer"));
 
             var cert = new X509Certificate2(Path.Combine(_environment.ApplicationBasePath, "idsrv4test.pfx"), "idsrv3test");
@@ -63,6 +64,19 @@ namespace IdSvrHost2
         {
             loggerFactory.AddConsole(LogLevel.Verbose);
             loggerFactory.AddDebug(LogLevel.Verbose);
+
+            var sourceEventSwitch = new SourceSwitch("LoggingSample");
+            sourceEventSwitch.Level = SourceLevels.Critical;
+            
+            loggerFactory.AddTraceSource(sourceEventSwitch,
+                new EventLogTraceListener("Application"));
+
+            var sourceFileSwitch = new SourceSwitch("LoggingSample");
+            sourceFileSwitch.Level = SourceLevels.All;
+
+            loggerFactory.AddTraceSource(sourceFileSwitch,
+                new TextWriterTraceListener(@"\log\IdSvrHost.log")
+            );
 
             app.UseDeveloperExceptionPage();
             app.UseIISPlatformHandler();
